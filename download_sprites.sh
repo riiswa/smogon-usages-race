e78bc24323491394a9064a1df31ad50c29923c3c
RADJI Waris <Waris.Radji@bordeaux-inp.fr>
2021-03-05 21:06:15 +0100

    Change directories & update scripts

--- download_sprites.sh
+++ download_sprites.sh
@@ -3,8 +3,8 @@
 url="https://play.pokemonshowdown.com/sprites/dex/"
 path="public/sprites/"
 
-arr=( $(jq -r '.[].pokemon' public/*.json) )
-for pkmn in $(printf '%s\n' "${arr[@]}" | uniq); do
-  pkmn=$(echo $pkmn | tr '[:upper:]' '[:lower:]' | tr -d ' ')
+PKMNS=( $(jq -r '.[].pokemon' public/data/*.json | uniq | tr -d ' ') )
+for pkmn in "${PKMNS[@]}"; do
+  pkmn=$(echo $pkmn | tr '[:upper:]' '[:lower:]')
   curl "$url${pkmn}.png" --create-dirs -o "$path${pkmn}.png"
 done
--- src/main/scala/com/warisradji/smogonusagesrace/RunExtractions.scala
+++ src/main/scala/com/warisradji/smogonusagesrace/RunExtractions.scala
@@ -6,7 +6,7 @@ import zio.{ExitCode, URIO, ZIO}
 object RunExtractions extends zio.App {
   def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ZIO.collectAllPar(Array[UsagesExtractor](
     UsagesExtractor(
-      "public/ou-1825.json",
+      "public/data/ou-1825.json",
       List(x => x.contains("ou"), x => x.contains("1825"), x => !x.contains("doubles")), 1
     )
   ).map(_.extract)).exitCode
